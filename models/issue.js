1eb7f5f45c439f58ce5a0f4995b4ee4e5172ea64
Replit user <>
2023-09-10 14:59:39 +0000

    update schema to fulfill project spec

--- models/issue.js
+++ models/issue.js
@@ -1,14 +1,32 @@
 const mongoose = require("mongoose");
 
 const issueSchema = new mongoose.Schema({
-  issue_title: String,
-  issue_text: String,
+  issue_title: {
+    type: String,
+    required: true
+  },
+  issue_text: {
+    type: String,
+    required: true
+  },
   created_on: String,
   updated_on: String,
-  created_by: String,
-  assigned_to: String,
-  open: Boolean,
-  status_text: String
+  created_by: {
+    type: String,
+    required: true
+  },
+  assigned_to: {
+    type: String,
+    required: false
+  },
+  open: {
+    type: Boolean,
+    default: true
+  },
+  status_text: {
+    type: String,
+    required: false
+  }
 })
 
 const Issue = mongoose.model("Issue", issueSchema);
