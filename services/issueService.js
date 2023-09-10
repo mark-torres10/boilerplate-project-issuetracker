7d956f80a7ce1ea62112c3c200cb5a365fba4cd8
Replit user <>
2023-09-10 15:12:48 +0000

    feat: create new issue in mongo

--- services/issueService.js
+++ services/issueService.js
@@ -1,1 +1,31 @@
 /* Manages direct access to MongoDB database */
+const { Issue } = require("../models/issue.js");
+const { getCurrentTimestampAsIsoString } = require("../lib/helper.js");
+
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+const MONGO_URI = process.env["MONGO_URI"];
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log("connected!!");
+
+
+/* MongoDB access functions */
+const createNewIssue = async ({ issue_title, issue_text, created_by, assigned_to="", status_text=""}) => {
+  const currentTimestampIsoString = getCurrentTimestampAsIsoString();
+
+  const newIssue = new Issue({
+    issue_title,
+    issue_text,
+    created_on: currentTimestampIsoString,
+    updated_on: currentTimestampIsoString,
+    created_by,
+    assigned_to,
+    open: true,
+    status_text
+  });
+
+  await newIssue.save();
+  return newIssue;
+}
+
