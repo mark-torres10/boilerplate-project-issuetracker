d188d49a563d67c64ecfd025010ecbb12019ecd9
Replit user <>
2023-09-10 14:56:52 +0000

    feat: move routes to controllers, add notes for what each request logic should do

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,8 @@
 'use strict';
+const issueController = require('../controllers/issueController');
 
-module.exports = function (app) {
+module.exports = function(app) {
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .put(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .delete(function (req, res){
-      let project = req.params.project;
-      
-    });
-    
+  issueController(app);
+
 };
