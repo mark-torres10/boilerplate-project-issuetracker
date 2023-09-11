653fa4d179f700c7b458f59c55ea6b561c2a8b77
Replit user <>
2023-09-11 03:17:13 +0000

    add tests for remaining endpoints

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,7 +14,7 @@ suite('Functional Tests', function() {
   const DEFAULT_ASSIGNED_TO = "";
   const MOCK_STATUS_TEXT = "In Progress";
   const DEFAULT_STATUS_TEXT = "";
-
+  
   suite("Testing POST requests", () => {
     test("Create an issue with every field: POST request to /api/issues/{project}", (done) => {
       chai
@@ -72,6 +72,211 @@ suite('Functional Tests', function() {
           assert.equal(res.body.error, "required field(s) missing.");
           done();
         })
+    });
+  });
+
+  suite("Testing GET requests", () => {
+    test("View issues on a project: GET request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .get("/api/issues/mock-project")
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body, 'Response should be an array');
+        // should be nonzero array
+        assert.isAtLeast(res.body.length, 1);
+        done();
+      });
+    });
+    test("View issues on a project with one filter: GET request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .get("/api/issues/mock-project")
+      .query({ issue_title: MOCK_ISSUE_TITLE })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body, 'Response should be an array');
+        // should be nonzero array
+        assert.isAtLeast(res.body.length, 1);
+        done();
+      });
+    });
+    test("View issues on a project with multiple filters: GET request to /api/issues/{project}", (done) => {
+    chai
+      .request(server)
+      .get("/api/issues/mock-project")
+      .query({
+        issue_title: MOCK_ISSUE_TITLE,
+        created_by: MOCK_CREATED_BY,
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body, 'Response should be an array');
+        // should be nonzero array
+        assert.isAtLeast(res.body.length, 1);
+        done();
+      });
+    });
+  });
+
+  suite("Testing PUT requests", () => {
+    test("Update one field on an issue: PUT request to /api/issues/{project}", (done) => {
+      // Create a new issue
+      const newIssue = await createNewIssue({
+        issue_title: "Title 1",
+        issue_text: "Text 1",
+        created_by: "User 1",
+      });
+      // Update one field (issue_text) on the issue
+      chai
+        .request(server)
+        .put("/api/issues/mock-project")
+        .send({
+          _id: newIssue._id,
+          issue_text: "Updated Text 1",
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.result, "successfully updated");
+          assert.equal(res.body._id, newIssue._id);
+          done();
+        });
+    });
+    test("Update multiple fields on an issue: PUT request to /api/issues/{project}", (done) => {
+      // Create a new issue
+      const newIssue = await createNewIssue({
+        issue_title: "Title 2",
+        issue_text: "Text 2",
+        created_by: "User 2",
+      });
+  
+      // Update multiple fields (issue_title and issue_text) on the issue
+      chai
+        .request(server)
+        .put("/api/issues/mock-project")
+        .send({
+          _id: newIssue._id,
+          issue_title: "Updated Title 2",
+          issue_text: "Updated Text 2",
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.result, "successfully updated");
+          assert.equal(res.body._id, newIssue._id);
+          done();
+        });
+      });
+    test("Update an issue with missing _id: PUT request to /api/issues/{project}", (done) => {
+      // Attempt to update an issue without providing an _id
+      chai
+        .request(server)
+        .put("/api/issues/mock-project")
+        .send({
+          issue_title: "Title 3",
+          issue_text: "Text 3",
+          created_by: "User 3",
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.property(res.body, "error");
+          assert.equal(res.body.error, "missing _id");
+          done();
+        });
+    });
+    test("Update an issue with no fields to update: PUT request to /api/issues/{project}", (done) => {
+      // Create a new issue
+      const newIssue = await createNewIssue({
+        issue_title: "Title 4",
+        issue_text: "Text 4",
+        created_by: "User 4",
+      });
+  
+      // Attempt to update an issue without providing any fields to update
+      chai
+        .request(server)
+        .put("/api/issues/mock-project")
+        .send({
+          _id: newIssue._id,
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.property(res.body, "error");
+          assert.equal(res.body.error, "no update field(s) sent");
+          assert.equal(res.body._id, newIssue._id);
+          done();
+        });
+    });
+    test("Update an issue with an invalid _id: PUT request to /api/issues/{project}", (done) => {
+      chai
+        .request(server)
+        .put("/api/issues/mock-project")
+        .send({
+          _id: "invalid_id",
+          issue_title: "Title 5",
+          issue_text: "Text 5",
+          created_by: "User 5",
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.property(res.body, "error");
+          assert.equal(res.body.error, "could not update");
+          assert.equal(res.body._id, "invalid_id");
+          done();
+        });
+    });
+  });
+
+  suite("Testing DELETE requests", () => {
+    test("Delete an issue: DELETE request to /api/issues/{project}", (done) => {
+      // Create a new issue
+      const newIssue = await createNewIssue({
+        issue_title: "Title 1",
+        issue_text: "Text 1",
+        created_by: "User 1",
+      });
+  
+      // Delete the created issue
+      chai
+        .request(server)
+        .delete("/api/issues/mock-project")
+        .send({
+          _id: newIssue._id,
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.result, "successfully deleted");
+          assert.equal(res.body._id, newIssue._id);
+          done();
+        });
+    });
+    test("Delete an issue with an invalid _id: DELETE request to /api/issues/{project}", (done) => {
+      // Attempt to delete an issue with an invalid _id
+      chai
+        .request(server)
+        .delete("/api/issues/mock-project")
+        .send({
+          _id: "invalid_id",
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.property(res.body, "error");
+          assert.equal(res.body.error, "could not delete");
+          assert.equal(res.body._id, "invalid_id");
+          done();
+        });
+    });
+    test("Delete an issue with missing _id: DELETE request to /api/issues/{project}", (done) => {
+      // Attempt to delete an issue without providing an _id
+      chai
+        .request(server)
+        .delete("/api/issues/mock-project")
+        .send({})
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.property(res.body, "error");
+          assert.equal(res.body.error, "missing _id");
+          done();
+        });
     });
   });
 });
